f513532b2d1a121de46a5e53a011ac314ef17f25
Yell Min Naing <yellminnaing5@gmail.com>
2026-06-25 15:12:15 +0630

    Create Program.cs

---
+++ src/CleanArch.WebApi/Program.cs
@@ -0,0 +1,42 @@
+using CleanArch.Application;
+using CleanArch.Infrastructure;
+using CleanArch.WebApi.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register Clean Architecture layers
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+// Add OpenAPI document generation
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    
+    // Initialise and seed database
+    using (var scope = app.Services.CreateScope())
+    {
+        var initializer = scope.ServiceProvider.GetRequiredService<CleanArch.Infrastructure.Persistence.ApplicationDbContextInitializer>();
+        await initializer.InitialiseAsync();
+        await initializer.SeedAsync();
+    }
+}
+
+// Global Custom Exception Handling Middleware
+app.UseMiddleware<ApiExceptionHandlingMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
