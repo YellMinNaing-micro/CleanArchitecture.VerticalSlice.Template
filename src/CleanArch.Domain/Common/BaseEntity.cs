5b01c2ab511803b368f5de5b42e848e060796ebe
Yell Min Naing <yellminnaing5@gmail.com>
2026-06-25 15:16:43 +0630

    Create BaseEntity.cs

---
+++ src/CleanArch.Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CleanArch.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
