2358c8be5ae191d327fb564fdede40a3a3826386
Yell Min Naing <yellminnaing5@gmail.com>
2026-06-25 15:32:45 +0630

    Update ProductsController.cs

--- src/CleanArch.WebApi/Controllers/ProductsController.cs
+++ src/CleanArch.WebApi/Controllers/ProductsController.cs
@@ -10,18 +10,24 @@ namespace CleanArch.WebApi.Controllers;
 public class ProductsController : ApiControllerBase
 {
     [HttpPost]
+    [EndpointSummary("Create a new product")]
+    [EndpointDescription("Creates a new product in the database and returns its unique ID.")]
     public async Task<ActionResult<int>> Create(CreateProductCommand command)
     {
         return await Mediator.Send(command);
     }
 
     [HttpGet]
+    [EndpointSummary("Get all products")]
+    [EndpointDescription("Retrieves a list of all products currently in the database.")]
     public async Task<ActionResult<IReadOnlyList<ProductDto>>> GetAll()
     {
         return Ok(await Mediator.Send(new GetProductsQuery()));
     }
 
     [HttpGet("{id}")]
+    [EndpointSummary("Get product details by ID")]
+    [EndpointDescription("Retrieves details of a specific product using its unique ID.")]
     public async Task<ActionResult<ProductDto>> GetById(int id)
     {
         var product = await Mediator.Send(new GetProductByIdQuery(id));
