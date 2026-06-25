e827507060a9dafa2a04106d629bfc7c96568ec2
Yell Min Naing <yellminnaing5@gmail.com>
2026-06-25 15:19:22 +0630

    Create ApplicationDbContext.cs

---
+++ src/CleanArch.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Price).HasPrecision(18, 2);
+        });
+    }
+}
