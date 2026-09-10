31e1b9cf05a18fe1012e74aaca6d2b9e8337a449
Yell Min Naing <yellminnaing5@gmail.com>
2026-09-10 11:43:10 +0630

    Update ApplicationDbContext.cs

--- src/CleanArch.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/CleanArch.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
+
         modelBuilder.Entity<Product>(entity =>
         {
             entity.HasKey(e => e.Id);
