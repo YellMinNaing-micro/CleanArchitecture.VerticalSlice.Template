f3363df485cf93a73193a896f94f88f2ef3209f2
Yell Min Naing <yellminnaing5@gmail.com>
2026-09-11 12:04:54 +0630

    Create CustomWebApplicationFactory.cs

---
+++ tests/CleanArch.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+using CleanArch.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace CleanArch.IntegrationTests;
+
+public sealed class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly SqliteConnection _connection = new("Data Source=:memory:");
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        _connection.Open();
+
+        builder.UseEnvironment("Testing");
+        builder.ConfigureServices(services =>
+        {
+            services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+            services.RemoveAll<ApplicationDbContext>();
+
+            services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+            {
+                options.UseSqlite(_connection);
+                options.AddInterceptors(serviceProvider.GetRequiredService<
+                    Infrastructure.Persistence.Interceptors.AuditableEntityInterceptor>());
+            });
+
+            using var scope = services.BuildServiceProvider().CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Database.EnsureCreated();
+        });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            _connection.Dispose();
+        }
+    }
+}
