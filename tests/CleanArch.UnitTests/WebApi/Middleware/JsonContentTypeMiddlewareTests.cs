c726442e451655f6df4eea6681ddbed1e21239c3
Yell Min Naing <yellminnaing5@gmail.com>
2026-09-15 14:11:24 +0630

    Update JsonContentTypeMiddlewareTests.cs

--- tests/CleanArch.UnitTests/WebApi/Middleware/JsonContentTypeMiddlewareTests.cs
+++ tests/CleanArch.UnitTests/WebApi/Middleware/JsonContentTypeMiddlewareTests.cs
@@ -48,7 +48,7 @@ public class JsonContentTypeMiddlewareTests
         context.Response.Body.Position = 0;
         using var document = await JsonDocument.ParseAsync(context.Response.Body);
         document.RootElement.GetProperty("success").GetBoolean().Should().BeFalse();
-        document.RootElement.GetProperty("message").GetString()
+        document.RootElement.GetProperty("message").GetProperty("en").GetString()
             .Should().Be("Request Content-Type must be application/json.");
     }
 
