8b9802079cf5e543309a8b714d3417fe5af35fd3
Yell Min Naing <yellminnaing5@gmail.com>
2026-09-15 13:55:18 +0630

    Update ApiExceptionHandlingMiddleware.cs

--- src/CleanArch.WebApi/Middleware/ApiExceptionHandlingMiddleware.cs
+++ src/CleanArch.WebApi/Middleware/ApiExceptionHandlingMiddleware.cs
@@ -1,6 +1,6 @@
 using FluentValidation;
+using CleanArch.WebApi.Helpers;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -38,37 +38,33 @@ public class ApiExceptionHandlingMiddleware
     {
         context.Response.ContentType = "application/json";
 
-        var problemDetails = new ProblemDetails();
+        string message;
+        IReadOnlyDictionary<string, string[]>? errors = null;
 
         if (exception is ValidationException validationException)
         {
             context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            problemDetails.Status = StatusCodes.Status400BadRequest;
-            problemDetails.Title = "Validation failed";
-            problemDetails.Detail = "One or more validation errors occurred.";
+            message = "One or more validation errors occurred.";
 
-            var errors = validationException.Errors
+            errors = validationException.Errors
                 .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
                 .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
 
-            problemDetails.Extensions.Add("errors", errors);
         }
         else if (exception is KeyNotFoundException keyNotFoundException)
         {
             context.Response.StatusCode = StatusCodes.Status404NotFound;
-            problemDetails.Status = StatusCodes.Status404NotFound;
-            problemDetails.Title = "Not Found";
-            problemDetails.Detail = keyNotFoundException.Message;
+            message = keyNotFoundException.Message;
         }
         else
         {
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            problemDetails.Status = StatusCodes.Status500InternalServerError;
-            problemDetails.Title = "Internal Server Error";
-            problemDetails.Detail = exception.Message;
+            message = "An unexpected error occurred.";
         }
 
-        var json = JsonSerializer.Serialize(problemDetails);
+        var json = JsonSerializer.Serialize(
+            ApiResponseHelper.Failure(message, errors),
+            new JsonSerializerOptions(JsonSerializerDefaults.Web));
         await context.Response.WriteAsync(json);
     }
 }
